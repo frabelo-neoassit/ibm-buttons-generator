8e112fb359e17cd418054f949f829665999e1711
Fabricio Pereira Rabelo <frabelo@neoassist.com>
2021-09-24 16:22:39 -0300

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# ibm-buttons-generator
+# IBM INTENTS GENERATOR
+
+Gera arquivo de botões do IBM Watson.
+
+## Requerimentos
+
+É necessário que você tenha o Node JS em seu ambiente de trabalho para que possa instalar as bibliotecas necessárias do script.
+
+### Como instalar as dependências?
+
+Você pode usar o npm ou yarn para instalar as dependências. Utilize um terminal (em ambiente Unix) ou prompt de comandos (em ambiente Windows):
+
+```bash
+yarn install ou npm install
+```
+
+### Como funciona?
+
+O script gera um JSON de botões, no formato esperado pelo IBM Watson. Para isso basta rodar o script **ibm-buttons** (em ambiente Unix) ou **ibm-buttons** (em ambiente Windows) na raiz do projeto.
+
+```bash
+./ibm-buttons --file=/full/path/of/buttons.txt
+```
+
+O seu arquivo de botões deve conter uma inteção por linha, pode ser um arquivo simples de texto. Ex.:
+
+```text
+Botão 1
+Botão 2
+Botão 3
+Botão X
+```
+
+No argumento **--file** passe o caminho completo do arquivo de botões.
+
+O arquivo de saída é gerado na pasta **/storage** da raíz do projeto.
---
+++ src/index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
+
+const file = argv.file;
+const encoding = "utf8";
+const filePath = path.join(__dirname, "..", "storage");
+
+if (!fs.existsSync(filePath)) {
+  fs.mkdirSync(filePath, { mode: "0755" });
+}
+
+if (file) {
+  if (file && fs.existsSync(file)) {
+    const fileContent = fs.readFileSync(file, {
+      encoding,
+      flag: "r",
+    });
+    const data = fileContent
+      .toString()
+      .trim()
+      .split("\n")
+      .map((value) => {
+        return { type: "sendMessage", label: value, value };
+      });
+
+    const fileName = `${uuidv4()}ca_buttons.json`;
+    fs.writeFileSync(
+      path.join(__dirname, "..", "storage", fileName),
+      JSON.stringify(data),
+      {
+        encoding: "utf8",
+        flag: "w",
+      }
+    );
+    console.log("File generated successfully!");
+  } else {
+    console.log("File does not exist!");
+  }
+} else {
+  console.log("File argument was not entered!");
+}
+
+process.exit(0);
